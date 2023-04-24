7edc34f113828e04aad0c5b05d339a7dc4aa3c55
Jacob Cole <jacob@spectrumnetdesigns.com>
2023-04-23 22:40:00 -0400

    ⚙️ Add settings menu and overflow

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,11 @@ import { clientSearch, runTerminalCommand } from "./commands";
 import { init as initPanels } from "./panels";
 
 export async function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerCommand('as2.start', () => runTerminalCommand(`pm2 delete all; rush update; rush build && rush start`, { name: `AS2 Project Start` })));
+	context.subscriptions.push(vscode.commands.registerCommand('as2.settings', () => vscode.commands.executeCommand('workbench.action.openSettings', 'Auctioneer Software')));
+	context.subscriptions.push(vscode.commands.registerCommand('as2.start', () => runTerminalCommand(`pm2 status; rush update; rush build && rush start`, { name: `AS2 Project Start`, ttl: 0 })));
+	context.subscriptions.push(vscode.commands.registerCommand('as2.logs', () => runTerminalCommand(`pm2 logs`, { name: `AS2 Logs` })));
 	context.subscriptions.push(vscode.commands.registerCommand('as2.coreUpdate', () => runTerminalCommand(`npx @snd/as2-update`, { name: `AS2 Core Update` })));
-	context.subscriptions.push(vscode.commands.registerCommand('as2.init', () => runTerminalCommand(`npx @snd/dev init`, { name: `AS2 Dev Init` })));
+	context.subscriptions.push(vscode.commands.registerCommand('as2.devInit', () => runTerminalCommand(`npx @snd/dev init`, { name: `AS2 Dev Init` })));
 	context.subscriptions.push(vscode.commands.registerCommand('as2.clients.search', clientSearch));
 	initPanels(context);
 }
