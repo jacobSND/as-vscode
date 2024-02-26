f3c0ef207777230eb0fa5ed832aa077f70052778
Jacob Cole <jacob@spectrumnetdesigns.com>
2024-02-26 11:05:52 -0500

    Prompt for init on new projects

--- src/utilities/init.ts
+++ src/utilities/init.ts
@@ -1,5 +1,6 @@
 import * as vscode from "vscode";
 import * as semver from 'semver';
+import { runTerminalCommand } from "../commands";
 
 export async function init(context: vscode.ExtensionContext) {
   const version = context.extension.packageJSON.version;
@@ -12,6 +13,29 @@ export async function init(context: vscode.ExtensionContext) {
     });
   }
   context.secrets.store('version', version);
+
+  const hideInitPrompt = await context.secrets.get('hideInitPrompt');
+  if (!hideInitPrompt) {
+    const workspaceFolderUri = vscode.workspace.workspaceFolders?.[0].uri;
+    const workspaceFolderName = workspaceFolderUri?.path.split('/').pop();
+    if (workspaceFolderName?.includes('-auctionsoftware') && workspaceFolderUri) {
+      try {
+        await vscode.workspace.fs.stat(vscode.Uri.joinPath(workspaceFolderUri, '.env'));
+      } catch {
+        const options = [
+          { label: 'Run dev-init', onClick: () => runTerminalCommand(`npx @snd/dev init`, { name: `AS2 Dev Init`, ttl: 0 }) },
+          { label: `Don't show again`, onClick: () => context.secrets.store('hideInitPrompt', 'true') },
+        ];
+        vscode.window.showInformationMessage(
+          'Would you like to initialize this Auctioneer Software project?',
+          ...(options.map(({ label }) => label))
+        ).then(clicked => {
+          const option = options.find(option => option.label === clicked);
+          option?.onClick();
+        });
+      }
+    }
+  }
 
   const settings = vscode.workspace.getConfiguration('as2.clients');
   if (settings?.pathOverrides || settings?.path_overrides) {
