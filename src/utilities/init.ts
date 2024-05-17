65bcd58aa125e26ee2890af1d575c1704185295a
Jacob Cole <jacob@spectrumnetdesigns.com>
2024-05-17 15:47:58 -0400

    More granular control for the init ignore settings

--- src/utilities/init.ts
+++ src/utilities/init.ts
@@ -1,6 +1,7 @@
 import * as vscode from "vscode";
 import * as semver from 'semver';
 import { runTerminalCommand } from "../commands";
+import { set } from "lodash";
 
 export async function init(context: vscode.ExtensionContext) {
   const version = context.extension.packageJSON.version;
@@ -14,17 +15,29 @@ export async function init(context: vscode.ExtensionContext) {
   }
   context.secrets.store('version', version);
 
-  const hideInitPrompt = await context.secrets.get('hideInitPrompt');
-  if (!hideInitPrompt) {
+  const settings = vscode.workspace.getConfiguration('as2.clients');
+  const overrides = JSON.parse(JSON.stringify(settings.overrides ?? {}));
+  if (!overrides['*']?.hideInitPrompt) {
     const workspaceFolderUri = vscode.workspace.workspaceFolders?.[0].uri;
     const workspaceFolderName = workspaceFolderUri?.path.split('/').pop();
     if (workspaceFolderName?.includes('-auctionsoftware') && workspaceFolderUri) {
+      const clientKey = workspaceFolderName.split('-')[0];
+      if (overrides[clientKey]?.hideInitPrompt) return;
       try {
         await vscode.workspace.fs.stat(vscode.Uri.joinPath(workspaceFolderUri, '.env'));
       } catch {
         const options = [
           { label: 'Run dev-init', onClick: () => runTerminalCommand(`npx @snd/dev init`, { name: `AS2 Dev Init`, ttl: 0 }) },
-          { label: `Don't show again`, onClick: () => context.secrets.store('hideInitPrompt', 'true') },
+          {
+            label: `Don't show again`, onClick: () => {
+              vscode.window.showInformationMessage('Hide for this Client or All Clients?', 'This Client', 'All Clients').then((value) => {
+                if (value) {
+                  set(overrides, `${value === 'This Client' ? clientKey : '*'}.hideInitPrompt`, true);
+                  settings.update('overrides', overrides, true);
+                }
+              });
+            }
+          }
         ];
         vscode.window.showInformationMessage(
           'Would you like to initialize this Auctioneer Software project?',
@@ -37,7 +50,6 @@ export async function init(context: vscode.ExtensionContext) {
     }
   }
 
-  const settings = vscode.workspace.getConfiguration('as2.clients');
   if (settings?.pathOverrides || settings?.path_overrides) {
     let overrides = JSON.parse(JSON.stringify(settings.overrides ?? {}));
     let deprecatedOverrides = JSON.parse(JSON.stringify(settings.pathOverrides ?? settings.path_overrides ?? {}));
